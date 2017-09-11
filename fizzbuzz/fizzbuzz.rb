821d9a20afbb556789183a1998984ff77fac80ac
pkbanks <pkbanks@gmail.com>
2017-09-11 16:27:27 -0400

    wrote basic tests, assumes argument provided is a number

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -3,4 +3,18 @@
 
 # Write a test and program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.”
 
+def fizz_buzz(num)
 
+	if num % 15 == 0
+		return "fizzbuzz"
+	end
+
+	if num % 5 == 0
+		return "buzz"
+	end
+
+	if num % 3 == 0
+		return "fizz"
+	end
+	return num
+end
--- fizzbuzz/spec/fizz_buzz_spec.rb
+++ fizzbuzz/spec/fizz_buzz_spec.rb
@@ -4,4 +4,33 @@ describe 'fizz_buzz' do
 	it 'returns 1 if number is 1' do
     expect(fizz_buzz(1)).to eq 1 
 	end
+
+	it "returns 'fizz' if number is divisible by 3" do
+		expect(fizz_buzz(3)).to eq "fizz"
+	end
+
+	it "returns 'buzz' if number is divisible by 5" do
+		expect(fizz_buzz(5)).to eq "buzz"
+	end
+
+	it "returns 'fizzbuzz' if number is divisible by 3 and 5" do
+		expect(fizz_buzz(15)).to eq "fizzbuzz"
+	end
+
+	it "returns 0" do
+		expect(fizz_buzz(0)).to eq "fizzbuzz"
+	end
+
+	it "returns 'fizzbuzz" do
+		expect(fizz_buzz(90)).to eq "fizzbuzz"
+	end
+
+	it "returns 'buzz'" do
+		expect(fizz_buzz(85)).to eq "buzz"
+	end
+
+	it "returns fizz" do
+		expect(fizz_buzz(33)).to eq "fizz"
+	end
 end
+
