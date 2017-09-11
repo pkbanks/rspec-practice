ed0937d70a38797a5a122cec0d923d8be8cad774
pkbanks <pkbanks@gmail.com>
2017-09-11 10:43:49 -0400

    adding fizzbuzz directory, starting game with tests

---
+++ fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,7 @@
+# See
+# https://medium.com/craft-academy/introduction-to-ruby-and-rspec-135da4051802
+
+# Write a test and program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.”
+
+require 'rspec'
+
