f3ed5952d368a89a7d7cd3e53f5e2c7eee5e8c90
pkbanks <pkbanks@gmail.com>
2017-09-13 13:25:22 -0400

    implementing tutorial guide, with new model tests on spec/models/auction_spec.rb

--- rspec-practice/app/models/auction.rb
+++ rspec-practice/app/models/auction.rb
@@ -1,2 +1,4 @@
 class Auction < ApplicationRecord
+	validates_presence_of :title
+
 end
---
+++ rspec-practice/spec/models/auction_spec.rb
@@ -0,0 +1,20 @@
+# see here for reference:
+# https://semaphoreci.com/community/tutorials/how-to-test-rails-models-with-rspec
+
+require 'rails_helper'
+
+RSpec.describe Auction, :type => :model do
+  it "is valid with valid attributes" do
+    expect(Auction.new).to be_valid
+  end
+
+  it "is not valid without a title" do
+  	auction = Auction.new(title: nil)
+  	expect(auction).to_not be_valid
+	end
+
+  it "is not valid without a title"
+  it "is not valid without a description"
+  it "is not valid without a start_date"
+  it "is not valid without a end_date"
+end
